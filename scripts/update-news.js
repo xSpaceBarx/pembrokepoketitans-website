3e1f509f7cd23960243a3cced50b9649b21c138e
xSpaceBarx <xspacebarx@msn.com>
2026-07-29 19:26:46 -0400

    Update update-news.js

--- scripts/update-news.js
+++ scripts/update-news.js
@@ -1,1 +1,33 @@
+const Parser = require("rss-parser");
+const fs = require("fs");
 
+const parser = new Parser();
+
+(async () => {
+
+const feed = await parser.parseURL(
+"RSS_FEED_GOES_HERE"
+);
+
+const news = feed.items.slice(0,5).map(item => ({
+
+title: item.title,
+
+link: item.link,
+
+date: item.pubDate,
+
+image:
+item.enclosure?.url || "",
+
+description:
+item.contentSnippet || ""
+
+}));
+
+fs.writeFileSync(
+"data/news.json",
+JSON.stringify({news},null,2)
+);
+
+})();
