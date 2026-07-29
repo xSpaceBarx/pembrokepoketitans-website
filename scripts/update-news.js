9e11819e231fcdd24b367caf1218512f1e264bd8
xSpaceBarx <xspacebarx@msn.com>
2026-07-29 19:52:12 -0400

    Update update-news.js

--- scripts/update-news.js
+++ scripts/update-news.js
@@ -5,9 +5,27 @@ const parser = new Parser();
 
 (async () => {
 
-    const feed = await parser.parseURL(
-        "https://pokemongohub.net/feed"
-    );
+const axios = require("axios");
+
+const response = await axios.get(
+    "https://api.rss2json.com/v1/api.json?rss_url=https://pokemongohub.net/feed"
+);
+
+const news = response.data.items.slice(0,5).map(item => ({
+
+    title: item.title,
+
+    link: item.link,
+
+    date: item.pubDate,
+
+    description: item.description
+        .replace(/<[^>]*>/g,"")
+        .substring(0,140) + "...",
+
+    image: item.thumbnail
+
+}));
 
     const news = feed.items
         .slice(0,5)
