d82b2df791785eb11aa6487ce5e8f744c94b3cb8
xSpaceBarx <xspacebarx@msn.com>
2026-08-05 17:10:57 -0400

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,19 @@
+document
+.getElementById("openNotifications")
+.addEventListener("click", () => {
 
+    alert(
+`🚧 Notification Center
+
+This will become your control panel for:
+
+• Meetups
+• GO Pass
+• Lucky Trinkets
+• Redemption Codes
+• Breaking News
+
+Next we'll build the actual sender.`
+    );
+
+});
