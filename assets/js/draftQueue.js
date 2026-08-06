7d2e48b2ed810afb5ced791fe7aa9388747d0e65
xSpaceBarx <xspacebarx@msn.com>
2026-08-05 21:27:18 -0400

    Update draftQueue.js

--- assets/js/draftQueue.js
+++ assets/js/draftQueue.js
@@ -1,6 +1,6 @@
-import { deleteDraft } from "./deleteDraft.js";
 import { db } from "./firebase.js";
 import { loadDraft } from "./editDraft.js";
+import { deleteDraft } from "./deleteDraft.js";
 
 import {
     collection,
@@ -54,21 +54,21 @@ export async function loadDrafts() {
                     ${draft.status}
                 </span>
 
-               <div class="draft-buttons">
+                <div class="draft-buttons">
 
-    <button
-        class="edit-draft"
-        data-id="${draft.id}">
-        ✏ Edit
-    </button>
+                    <button
+                        class="edit-draft"
+                        data-id="${draft.id}">
+                        ✏ Edit
+                    </button>
 
-    <button
-        class="delete-draft"
-        data-id="${draft.id}">
-        🗑 Delete
-    </button>
+                    <button
+                        class="delete-draft"
+                        data-id="${draft.id}">
+                        🗑 Delete
+                    </button>
 
-</div>
+                </div>
 
             </div>
 
@@ -76,6 +76,7 @@ export async function loadDrafts() {
 
         });
 
+        // Edit buttons
         document.querySelectorAll(".edit-draft").forEach(button => {
 
             button.addEventListener("click", () => {
@@ -86,18 +87,21 @@ export async function loadDrafts() {
 
         });
 
+        // Delete buttons
+        document.querySelectorAll(".delete-draft").forEach(button => {
+
+            button.addEventListener("click", async () => {
+
+                await deleteDraft(button.dataset.id);
+
+                loadDrafts();
+
+            });
+
+        });
+
     }
-document.querySelectorAll(".delete-draft").forEach(button=>{
 
-    button.addEventListener("click",async()=>{
-
-        await deleteDraft(button.dataset.id);
-
-        loadDrafts();
-
-    });
-
-});
     catch (err) {
 
         console.error(err);
