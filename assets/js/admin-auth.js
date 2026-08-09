b6ed28c5f3841653759b53c440abd440ec6835ea
xSpaceBarx <xspacebarx@msn.com>
2026-08-09 19:06:15 -0400

    Update admin-auth.js

--- assets/js/admin-auth.js
+++ assets/js/admin-auth.js
@@ -105,7 +105,7 @@ async function openAdmin(user) {
          * AFTER authorization succeeds.
          */
         await import(
-            "./admin.js?v=15"
+            "./admin.js?v=16"
         );
 
 
